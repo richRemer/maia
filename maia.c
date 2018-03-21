3bbdda755e4aa2d3e7ccb56fcd663e4f75bc28d0
Rich Remer <rich.remer@gmail.com>
2018-03-21 11:39:51 -0700

    run in separate processes

--- maia.c
+++ maia.c
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
 	gdk_init(&argc, &argv);
 	enable_styles("style/maia.css");
 
-	app = gtk_application_new("us.remer.maia", G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new("us.remer.maia", G_APPLICATION_NON_UNIQUE);
 	g_signal_connect(app, "activate", G_CALLBACK(app_activated), NULL);
 
 	status = g_application_run(G_APPLICATION(app), argc, argv);
